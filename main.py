78ef1a5326337716632736e490d9cf04ffc75886
gewoondim1 <dimscheurink@hotmail.com>
2018-04-27 13:58:51 +0200

    Created PCA class
    todo: create a plot function for the PCA class

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
+
+
+class PCA:
+    def __init__(self, dataframe, columns=None, startvar=0, endvar=0):
+        self.df = dataframe
+
+        if columns:
+            self.x = np.array(dataframe)[:, columns].astype(np.float32)
+        elif not endvar:
+            self.x = np.array(dataframe)[:, startvar:].astype(np.float32)
+        else:
+            self.x = np.array(dataframe)[:, startvar:endvar].astype(np.float32)
+
+    def normalize(self, normalize=True):
+        self.x -= self.x.mean(axis=0)
+
+        if normalize:
+            self.x /= self.x.std(axis=0)
+
+    def addpca(self, n=2):
+        covmatrix = np.dot(self.x.transpose(), self.x) / (len(self.x) - 1)
+        lambdacov, pcov = [list(e) for e in np.linalg.eig(covmatrix)]
+
+        lambdacovsorted = sorted(lambdacov)
+        indices = (lambdacov.index(i) for i in lambdacovsorted[:n])
+
+        for i, eig in enumerate(indices, 1):
+            self.df["PC" + str(i)] = np.dot(self.x, pcov[eig])
 
 
 filelocation = "https://www.dropbox.com/s/4xomtdd3tee0efg/Senseo%20koffie.csv?raw=1"
@@ -9,40 +37,24 @@ filelocation = "https://www.dropbox.com/s/4xomtdd3tee0efg/Senseo%20koffie.csv?ra
 df = read_csv(filelocation, delimiter=";")
 print(list(df))  # columns
 
-x = np.array(df)[:, 4:].astype(np.float32)
-
-"""
-_, ax = plt.subplots()
-ax.boxplot(x.transpose().tolist())
-plt.show()
-"""
-
-# Normalize the data
-xc = x - x.mean(axis=0)
-xcs = xc / xc.std(axis=0)
-
-covMatrix = np.dot(xcs.transpose(), xcs) / (len(xcs)-1)
-lambdaCov, pCov = [list(e) for e in np.linalg.eig(covMatrix)]
-
-lambdaCovSorted = sorted(lambdaCov)
-indices = (lambdaCov.index(i) for i in lambdaCovSorted[:2])
-df["PC1"], df["PC2"] = (np.dot(xcs, pCov[i]) for i in indices)
-
+x = PCA(df, startvar=4)
+x.normalize()
+x.addpca()
 
 plt.figure(figsize=(5, 5))
 customPalette = ['#630C3A', '#39C8C6', '#D3500C', '#FFB139']
 
 # loop through labels and plot each cluster
-for i, label in enumerate(df["Blend"].unique()):
+for i, label in enumerate(x.df["Blend"].unique()):
     # add data points 
-    plt.scatter(x=df.loc[df["Blend"] == label, "PC1"],
-                y=df.loc[df["Blend"] == label, "PC2"],
+    plt.scatter(x=x.df.loc[x.df["Blend"] == label, "PC1"],
+                y=x.df.loc[x.df["Blend"] == label, "PC2"],
                 color=customPalette[i],
                 alpha=0.50)
 
     # add label
     plt.annotate(label,
-                 df.loc[df["Blend"] == label, ["PC1", "PC2"]].mean(),
+                 x.df.loc[x.df["Blend"] == label, ["PC1", "PC2"]].mean(),
                  horizontalalignment='center',
                  verticalalignment='center',
                  size=15, weight='bold',
